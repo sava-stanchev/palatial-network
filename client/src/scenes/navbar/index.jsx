d40c36ef9f1880abd5dec504d087b4f52e824462
Sava Stanchev <s.stanchev94@gmail.com>
2023-05-23 22:05:21 +0300

    wrapper & user img - widget helpers + isAuth

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
   const background = theme.palette.background.default;
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
-  const fullName = `Sava Stanchev`;
+  const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
